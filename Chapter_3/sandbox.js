d8f73a7e824b66595f704f196439c53f7d3c20ed
Sirvan Cheraghi <78662653+SirvanCheraghi@users.noreply.github.com>
2022-08-08 22:22:52 +0430

    Update sandbox.js

--- Chapter_3/sandbox.js
+++ Chapter_3/sandbox.js
@@ -1,6 +1,6 @@
 // for loop
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+for (let index = 1; index < 6; index++) {
+    console.log(`loop ${index}`);
     
 };
